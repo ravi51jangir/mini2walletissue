bec0c25627d3bfc7f5af4aa1d48d251b8b1326a0
Mavrick <vivekkhudaniya@gmail.com>
2024-12-02 23:10:07 +0530

    Mini app routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createWeb3Modal } from "@web3modal/wagmi/react";
-
 import { http, createConfig, WagmiProvider } from "wagmi";
 import { mainnet, arbitrum } from "viem/chains";
 import { walletConnect, coinbaseWallet, injected } from "wagmi/connectors";
 import type { CreateConnectorFn } from '@wagmi/core'
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { authConnector,  } from "@web3modal/wagmi";
+import { authConnector } from "@web3modal/wagmi";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Import your pages
 import Homepage from "./pages/HOMEPAGE";
 import Homepage_2 from "./pages/HOMEPAGE_2";
 import Homepage_3 from "./pages/HOMEPAGE_3";
@@ -23,7 +24,7 @@ import "./styles.css"
 // 0. Setup queryClient
 const queryClient = new QueryClient();
 
-// 1. Get projectId at https://cloud.walletconnect.com
+// 1. Get projectId
 const projectId = import.meta.env.VITE_PROJECT_ID;
 if (!projectId) throw new Error("Project ID is undefined");
 
@@ -38,7 +39,7 @@ const metadata = {
 // Define chains
 const chains = [mainnet, arbitrum] as const
 
-// create the connectors
+// Create connectors
 const connectors: CreateConnectorFn[] = []
 connectors.push(walletConnect({ projectId, metadata, showQrModal: false }));
 connectors.push(injected({ shimDisconnect: true }));
@@ -49,14 +50,14 @@ connectors.push(coinbaseWallet({
 
 connectors.push(authConnector({ 
   options: { projectId },
-  socials: ['google', 'x', 'github', 'discord', 'apple'], // this will create a non-custodial wallet (please check https://secure.walletconnect.com/dashboard for more info)
+  socials: ['google', 'x', 'github', 'discord', 'apple'],
   showWallets: true,
   email: true,
   walletFeatures: false,
 }));
 
 const wagmiConfig = createConfig({
-  chains, // Use the defined chains here
+  chains,
   transports: {
     [mainnet.id]: http(),
     [arbitrum.id]: http(),
@@ -67,23 +68,36 @@ const wagmiConfig = createConfig({
 // 3. Create modal
 createWeb3Modal({ wagmiConfig, projectId });
 
+// Create App component with routing
+function App() {
+  return (
+    <div className="centered-div">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/home2" element={<Homepage_2 />} />
+        <Route path="/home3" element={<Homepage_3 />} />
+        <Route path="/nft" element={<Nftpage />} />
+        <Route path="/stack" element={<Stackpage />} />
+        <Route path="/notifications" element={<Notificationpage />} />
+        <Route path="/ico3" element={<ICO_3page />} />
+        <Route path="/ico4" element={<ICO_4page />} />
+        <Route path="/ico5" element={<ICO_5page />} />
+        {/* Redirect any unknown routes to home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <w3m-button />
+    </div>
+  );
+}
+
+// Root render
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <div className="centered-div">
-          {/* <w3m-button /> */}
-          {/* <Homepage/> */}
-          <Homepage_2/>
-          {/* <Homepage_3/> */}
-          {/* <Nftpage/> */}
-          {/* <Stackpage/> */}
-          {/* <Notificationpage/> */}
-          {/* <ICO_3page/> */}
-          {/* <ICO_4page/> */}
-          {/* <ICO_5page/> */}
-          
-        </div>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>
